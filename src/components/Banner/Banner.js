a802febcfb10addb7cd97016900735e46e2a8c6b
Rokan Uddin <rokanu55@gmail.com>
2021-12-04 02:04:01 +0600

    homepage design updated

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,11 +8,11 @@ const Banner = () => {
               <div className="col-lg-6 col-sm-12"></div>
               <div className="col-lg-6 col-sm-12 d-flex justify-content-center align-items-center">
               <div>
-              <h1 className="mt-5 text-white">Zoom Teeth Whitening</h1>
+              <h1 className="mt-5 text-white banner-heading">Zoom Teeth Whitening</h1>
               <p className="p-5 fs-5 text-white">Teeth whitening that's fast, safe, and reliable. 
                 This procedure is performed by one of our dentists or dental hygienists. 
                 Book your teeth-whitening appointment today and enjoy instant results.</p>
-                <Link to='/appoinment' className="myButton" >Make and Appoinment</Link>
+                <Link to='/appoinment' className="myButton" >Make Appoinment</Link>
               </div>
               </div>
         </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,31 +4,32 @@ import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import useAuth from '../../hooks/useAuth';
-
+import logo from '../../images/Dental.svg';
+import './Header.css';
 const Header = () => {
     // get user and logout using hooks
     const {user,logOut}= useAuth();
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
         <Container>
-        <Navbar.Brand as={Link} to="/home">Dental-Care</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/home"> <img className="title-logo" src={logo} alt="logo" /> <span className="title-1st">Dental</span> <span className="title-2nd">Care</span> </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-            <Nav.Link as={HashLink} to="/home#services">Services</Nav.Link>
-            <Nav.Link as={HashLink} to="/home#tips">Tips</Nav.Link>
-            <Nav.Link as={HashLink} to="/doctors">Doctors</Nav.Link>
-            <Nav.Link as={Link} to="/appoinment">Appoinment</Nav.Link>
+            <Nav.Link as={HashLink} to="/home#services" className="nav-link" >Services</Nav.Link>
+            <Nav.Link as={HashLink} to="/home#tips" className="nav-link">Tips</Nav.Link>
+            <Nav.Link as={HashLink} to="/doctors" className="nav-link">Doctors</Nav.Link>
+            <Nav.Link as={Link} to="/appoinment" className="nav-link">Appoinment</Nav.Link>
             </Nav>
             {/* if user's email available, Logout button displayed otherwise Sign-In button  */}
             <Nav>
             {
                 user.email ? 
-                <Button onClick={logOut} variant="light">Logout <i className="fas fa-sign-out-alt"></i> </Button> :
-                <Nav.Link as={Link} to="/login">Sign-In <i className="fas fa-sign-in-alt"></i> </Nav.Link>
+                <Button onClick={logOut} variant="light"  className="nav-link logout-btn" >Logout <i className="fas fa-sign-out-alt"></i> </Button> :
+                <Nav.Link as={Link} to="/login" className="nav-link login-btn">Sign-In <i className="fas fa-sign-in-alt"></i> </Nav.Link>
             }
             </Nav>
-            <Nav.Link>{user?.displayName}</Nav.Link>
+            <Nav.Link className="nav-link">{user?.displayName}</Nav.Link>
         </Navbar.Collapse>
         </Container>
         </Navbar>
